883793bf6ceeb44b8732f000ff8f24e8043b0683
Rocco Tripaldi <tripaldir@gmail.com>
2018-07-06 14:57:03 -0400

    use comma separation for array types

--- client/state/activity-log/utils.js
+++ client/state/activity-log/utils.js
@@ -17,21 +17,21 @@ export const filterStateToApiQuery = filter =>
 export const filterStateToQuery = filter =>
 	Object.assign(
 		{},
-		filter.action && { action: filter.action },
+		filter.action && { action: filter.action.join( ',' ) },
 		filter.after && { after: filter.after },
 		filter.before && { before: filter.before },
 		filter.by && { by: filter.by },
 		filter.date_range && { date_range: filter.date_range },
 		filter.group && filter.group.includes && { group: filter.group.includes.join( ',' ) },
 		filter.group && filter.group.excludes && { not_group: filter.group.excludes( ',' ) },
-		filter.name && { name: filter.name },
+		filter.name && { name: filter.name.join( ',' ) },
 		filter.page > 1 && { page: filter.page }
 	);
 
 export const queryToFilterState = query =>
 	Object.assign(
 		{},
-		query.action && { action: query.action },
+		query.action && { action: decodeURI( query.action ).split( ',' ) },
 		query.after && { after: query.after },
 		query.before && { before: query.before },
 		query.by && { by: query.by },
@@ -39,7 +39,7 @@ export const queryToFilterState = query =>
 		query.group && {
 			group: Object.assign( {}, { includes: decodeURI( query.group ).split( ',' ) } ),
 		},
-		query.name && { name: query.name },
+		query.name && { name: decodeURI( query.name ).split( ',' ) },
 		query.not_group && {
 			group: Object.assign( {}, { excludes: decodeURI( query.not_group ).split( ',' ) } ),
 		},
