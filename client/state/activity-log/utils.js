f75b64a78fd5a82ac671e3b6e5c43f5651aa45df
Rocco Tripaldi <tripaldir@gmail.com>
2018-07-06 10:24:02 -0400

    adds support for new filters based on recent api changes.

--- client/state/activity-log/utils.js
+++ client/state/activity-log/utils.js
@@ -3,22 +3,39 @@
 export const filterStateToApiQuery = filter =>
 	Object.assign(
 		{},
-		{ number: 1000 },
-		filter.group && filter.group.includes && { group: filter.group.includes }
+		filter.after && { after: filter.after },
+		filter.before && { before: filter.before },
+		filter.by && { by: filter.by },
+		filter.date_range && { date_range: filter.date_range },
+		filter.group && filter.group.includes && { group: filter.group.includes },
+		filter.group && filter.group.excludes && { not_group: filter.group.excludes },
+		{ number: 1000 }
 	);
 
 export const filterStateToQuery = filter =>
 	Object.assign(
 		{},
-		filter.page > 1 && { page: filter.page },
-		filter.group && filter.group.includes && { group: filter.group.includes.join( ',' ) }
+		filter.after && { after: filter.after },
+		filter.before && { before: filter.before },
+		filter.by && { by: filter.by },
+		filter.date_range && { date_range: filter.date_range },
+		filter.group && filter.group.includes && { group: filter.group.includes.join( ',' ) },
+		filter.group && filter.group.excludes && { not_group: filter.group.excludes( ',' ) },
+		filter.page > 1 && { page: filter.page }
 	);
 
 export const queryToFilterState = query =>
 	Object.assign(
 		{},
-		query.page && query.page > 0 && { page: query.page },
+		query.after && { after: query.after },
+		query.before && { before: query.before },
+		query.by && { by: query.by },
+		query.date_range && { date_range: query.date_range },
 		query.group && {
 			group: Object.assign( {}, { includes: decodeURI( query.group ).split( ',' ) } ),
-		}
+		},
+		query.not_group && {
+			group: Object.assign( {}, { excludes: decodeURI( query.not_group ).split( ',' ) } ),
+		},
+		query.page && query.page > 0 && { page: query.page }
 	);
