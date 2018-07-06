7ae900a7e3eaf7d41a858f87ba28aca761d9b1b0
Rocco Tripaldi <tripaldir@gmail.com>
2018-07-06 14:22:27 -0400

    Adding `name` and `action` filters

--- client/state/activity-log/utils.js
+++ client/state/activity-log/utils.js
@@ -3,30 +3,35 @@
 export const filterStateToApiQuery = filter =>
 	Object.assign(
 		{},
+		filter.action && { action: filter.action },
 		filter.after && { after: filter.after },
 		filter.before && { before: filter.before },
 		filter.by && { by: filter.by },
 		filter.date_range && { date_range: filter.date_range },
 		filter.group && filter.group.includes && { group: filter.group.includes },
 		filter.group && filter.group.excludes && { not_group: filter.group.excludes },
+		filter.name && { name: filter.name },
 		{ number: 1000 }
 	);
 
 export const filterStateToQuery = filter =>
 	Object.assign(
 		{},
+		filter.action && { action: filter.action },
 		filter.after && { after: filter.after },
 		filter.before && { before: filter.before },
 		filter.by && { by: filter.by },
 		filter.date_range && { date_range: filter.date_range },
 		filter.group && filter.group.includes && { group: filter.group.includes.join( ',' ) },
 		filter.group && filter.group.excludes && { not_group: filter.group.excludes( ',' ) },
+		filter.name && { name: filter.name },
 		filter.page > 1 && { page: filter.page }
 	);
 
 export const queryToFilterState = query =>
 	Object.assign(
 		{},
+		query.action && { action: query.action },
 		query.after && { after: query.after },
 		query.before && { before: query.before },
 		query.by && { by: query.by },
@@ -34,6 +39,7 @@ export const queryToFilterState = query =>
 		query.group && {
 			group: Object.assign( {}, { includes: decodeURI( query.group ).split( ',' ) } ),
 		},
+		query.name && { name: query.name },
 		query.not_group && {
 			group: Object.assign( {}, { excludes: decodeURI( query.not_group ).split( ',' ) } ),
 		},
